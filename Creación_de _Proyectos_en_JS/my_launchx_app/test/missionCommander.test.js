a3628b224776340e46601b7181dfd85166204b81
skrillheaven <beto.jacp2010@gmail.com>
2022-06-03 16:44:43 -0500

    primera prueba y correcciones

--- Creación_de _Proyectos_en_JS/my_launchx_app/test/missionCommander.test.js
+++ Creación_de _Proyectos_en_JS/my_launchx_app/test/missionCommander.test.js
@@ -1,6 +1,6 @@
 describe("Esto es una suite de pruebas", () => {
     test('Caso de prueba 1', () => {
-        const result = 1 + 2
+        const result = 5 + 5
         expect(result).toBe(10);
     });
 })
