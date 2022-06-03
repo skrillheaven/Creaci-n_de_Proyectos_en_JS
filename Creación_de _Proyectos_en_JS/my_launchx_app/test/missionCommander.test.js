099e1b4a69192dda0245ca6ef95f7cf9204d99f8
skrillheaven <beto.jacp2010@gmail.com>
2022-06-03 17:34:23 -0500

    ON CONFÍES EN UNA PRUEBA QUE NO HAS VISTO FALLAR

--- Creación_de _Proyectos_en_JS/my_launchx_app/test/missionCommander.test.js
+++ Creación_de _Proyectos_en_JS/my_launchx_app/test/missionCommander.test.js
@@ -3,6 +3,6 @@ const MissionCommander = require('./../app/missionComander');
 describe("Unit test for Mission Commander Class", () => {
     test('1). Create a mission commander object ', () => {
         const myMissionCommander = new MissionCommander("Woopa")
-        expect(myMissionCommander.name).toBe("Beto");
+        expect(myMissionCommander.name).toBe("Woopa");
     });
 })
